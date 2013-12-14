f5f65246cb0221e5dec12d58e1358e265be4f55a
Jesse Hattabaugh <arkanciscan@gmail.com>
2013-12-13 17:13:41 -0800

    init

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var gzippo = require("gzippo"),
-var app = express();
+var express = require("express"),
+  logfmt = require("logfmt"),
+  gzippo = require("gzippo")
+  app = express(),
+  port = process.env.PORT || 5000;
 
 app.use(logfmt.requestLogger());
 app.use(gzippo.staticGzip("" + __dirname + "/app"));
-
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
